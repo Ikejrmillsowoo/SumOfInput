b926b0906fe5664fb7c588d1cfb05a65633910f9
Isaac <isaac@Zipcoders-MacBook-Pro-2.local>
2025-02-27 13:24:08 -0500

    completed sumOfInput and gaussianSumOfNumbers with times calculated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
@@ -5,24 +7,43 @@
 public class Main {
 
     public static void main(String[] args) {
+        long startTime, endTime, elapsedTime;
         int number = getInputNumber();
 
+        startTime = System.currentTimeMillis();
         int sum = sumOfNumbers(number);
+        endTime = System.currentTimeMillis();
+        elapsedTime = endTime - startTime;
+        System.out.println("Loop 1 elapsed time: " + elapsedTime + " ms");
+
+
+        startTime = System.currentTimeMillis();
+        int sum2 = gaussianSumOfNumbers(number);
+        endTime = System.currentTimeMillis();
+        elapsedTime = endTime - startTime;
+        System.out.println("Loop 2 elapsed time: " + elapsedTime + " ms");
+
 
         System.out.println(sum);
+        System.out.println(sum2);
     }
 
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+
+        int sum = 0;
+        for (int i = 1; i <= n ; i++) {
+            sum = sum +i;
+        }
+        return sum;
     }
 
     boolean testSumOfNumbers() {
         return sumOfNumbers(3) == 6; // 3 + 2 + 1 + 0 = 6
     }
 
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+     static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -35,8 +56,8 @@ public class Main {
     }
 
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        return (n*(n+1))/2;
     }
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
